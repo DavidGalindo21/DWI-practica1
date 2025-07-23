e04438142592edd1e8a66c1031e042cb55fdcfed
DavidGalindo21 <2236000081@utna.edu.mx>
2025-07-22 18:47:43 -0600

    API con endpoints completos

--- app/controllers/palapaController.js
+++ app/controllers/palapaController.js
@@ -44,6 +44,7 @@ export const agregar = (req, res) => {
 };
 
 export const buscarBebida = (req, res, next) => { // debe llevar una llave y valor
+  if(!req.body)req.body = {}
   try {
     let consulta = {};
     consulta[req.params.key] = req.params.value;
@@ -70,4 +71,32 @@ export const mostrarBebida = (req, res) => {
   if(!req.body.bebidas) return res.status(204).json({ message: "No hay información que mostrar" });
   let bebidas = req.body.bebidas;
   return res.status(200).json(bebidas);
+}
+
+export const eliminarBebida = (req, res) => {
+  let bebidas = {}
+  bebidas = req.body.bebidas
+  Palapa.deleteOne(bebidas[0])
+  .then(info =>{
+    return res.status(200).json({ message: "Producto eliminado correctamente", info });
+  }).catch(e=>{
+    return res.status(404).json({ message: "Error al eliminar el producto", error: e });
+  })
+}
+
+export const actualizarBebida = (req, res) => {
+  let bebidas = {}
+  bebidas = req.body.bebidas;
+  if (!bebidas || bebidas.length === 0) {
+    return res.status(204).json({ message: "No se encontró la bebida a actualizar" });
+  }
+  
+  const updatedData = req.body; 
+  Palapa.updateOne(bebidas[0], updatedData)
+    .then(() => {
+      return res.status(200).json({ message: "Producto actualizado correctamente" });
+    })
+    .catch((error) => {
+      return res.status(404).json({ message: "Error al actualizar el producto", error });
+    });
 }
--- app/routes/palapaRouter.js
+++ app/routes/palapaRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { buscarTodo, agregar, buscarBebida, mostrarBebida } from "../controllers/palapaController.js";
+import { buscarTodo, agregar, buscarBebida, mostrarBebida,eliminarBebida, actualizarBebida } from "../controllers/palapaController.js";
 const router = express.Router();
 
 router.get("/bebidas", buscarTodo);
 router.post("/bebidas", agregar);
 router.get("/bebidas/:key/:value", buscarBebida, mostrarBebida);
+router.delete("/bebidas/:key/:value",buscarBebida,eliminarBebida)
+router.put("/bebidas/:key/:value",buscarBebida,actualizarBebida)
 
 export default router;
